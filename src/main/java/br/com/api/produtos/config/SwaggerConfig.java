fa05f5f63ee4bffc95278c4507e38b6e74e63799
Lucas Medeiros <lucasmmedeirosm@gmail.com>
2023-04-03 11:22:21 -0300

    inserção Swagger

---
+++ src/main/java/br/com/api/produtos/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package br.com.api.produtos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Conquest API REST",
+                "API REST de registro de conquistas.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Lucas Medeiros", "https://www.linkedin.com/in/lucas-medeiros-233328221/",
+                        "lucasmmedeirosm@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+}
--- src/main/java/br/com/api/produtos/controller/HobbiesController.java
+++ src/main/java/br/com/api/produtos/controller/HobbiesController.java
@@ -1,5 +1,7 @@
 package br.com.api.produtos.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,26 +19,31 @@ import br.com.api.produtos.service.HobbiesService;
 
 @RestController
 @CrossOrigin(origins = "*")
+@Api(value = "API REST Conquest")
 public class HobbiesController {
 
     @Autowired
     private HobbiesService ps;
 
+    @ApiOperation(value = "Remove um Hobbie já registrado")
     @DeleteMapping("/remover/hobbies/{id}")
     public ResponseEntity<RespostaModel> remover(@PathVariable long id) {
         return ps.remover(id);
     }
 
+    @ApiOperation(value = "Altera um Hobbie já registrado")
     @PutMapping("/alterar/hobbies")
     public ResponseEntity<?> alterar(@RequestBody HobbiesModel pm) {
         return ps.cadastrarAlterar(pm, "alterar");
     }
 
+    @ApiOperation(value = "Registra um Hobbie novo")
     @PostMapping("/cadastrar/hobbies")
     public ResponseEntity<?> cadastrar(@RequestBody HobbiesModel pm) {
         return ps.cadastrarAlterar(pm, "cadastrar");
     }
 
+    @ApiOperation(value = "Lista todos os Hobbies existentes")
     @GetMapping("/listar/hobbies")
     public Iterable<HobbiesModel> listar() {
         return ps.listar();
